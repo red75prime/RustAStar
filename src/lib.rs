f23f9e18ccc3af1c59dd2dd982ce5ea373027918
red75prime <red75prim@gmail.com>
2016-04-07 17:26:12 +0500

    Some docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! # AStar library
+//!
+//! AStar library provides implementation of A* algorithm.
+//! Library interface is functional (no OOP).
+
 extern crate typed_arena;
 
 use typed_arena::Arena as TypedArena;
@@ -36,31 +41,99 @@ impl<'a,A,S> Ord for FrontierElem<'a,A,S> {
     }
 }
 
+/// Copies action fields from single-linked list to array in reverse order.
+fn collect_actions<A,S>(fel: &Rc<FrontierElem<A,S>>) -> Vec<A>
+    where A:Clone {
+    let mut ret=Vec::<A>::with_capacity(16);
+    let mut cur=fel;
+    while let Some(ref prev)=cur.prev {
+        if let Some(ref action)=cur.action {
+            ret.push(action.clone());
+        };
+        cur=prev;
+    }
+    (&mut ret[..]).reverse();
+    ret
+}
+
+/// Contains algorithm's options. Deprecated. Use `astar_opt_ex` function
+///
+#[derive(Debug,Clone,Copy)]
+pub enum AstarOpt {
+    /// Default options. Same as `AOMaxFrontierStateCap(std::usize::MAX, 128)`
+    AONone, 
+    /// Sets maximum size of frontier queue. Search functions produce `Err(FrontierLimit(_))` on overflow. Default value `std::usize::MAX`, effectively unbounded.
+    AOMaxFrontier(usize),
+    /// Sets initial size of states' arena. Default value 128. Suggested to be roughly equal to the expected count of explored states.
+    AOStateCap(usize),
+    /// Sets both maximum size of frontier queue and initial size of states' arena.
+    AOMaxFrontierStateCap(usize,usize), 
+}
+
 use AstarOpt::{AONone, AOMaxFrontier, AOStateCap, AOMaxFrontierStateCap};
 
-pub enum AstarOpt {
-    AONone,
-    AOMaxFrontier(usize),
-    AOStateCap(usize),
-    AOMaxFrontierStateCap(usize,usize),
-}
-
-use AstarError::{NoPath, FrontierLimit};
-
+/// Search errors
+///
 #[derive(Debug,PartialEq,Eq)]
 pub enum AstarError<A> {
+    /// Path doesn't exist.
     NoPath,
+    /// Size of frontier queue exceeded limit. Contains partial path, having minimal expected total cost.
     FrontierLimit(Vec<A>),
+    /// Count of explored states exceeded limit. Contains partial path, having minimal expected total cost.
+    StateLimit(Vec<A>),
+    /// Heuristic cost function returned NaN.
+    NanCost,
 }
 
-/// 
-pub fn astar<S,A,Fnxt,Fend,Fheu>(s0 : S, fnxt : Fnxt, fend : Fend, fheu : Fheu) -> Result<Vec<A>,AstarError<A>>
-    where S : Ord, // State must implement Ord trait for BinaryHeap and BTreeSet
-        A : Clone, // Actions are supposed to be lightweight, thus Clone
-        Fnxt : Fn(&S) -> Vec<(A,S,f32)>, // Action generator function returns vector of triples (action, successor state, cost)
-        Fend : Fn(&S) -> bool, // Destination test function returns true for goal states
-        Fheu : Fn(&S) -> f32  { // Estimated cost of path from the state to a goal. Cost heuristic function should be admissible and consistent
-    astar_opt(s0, fnxt, fend, fheu, AONone)
+use AstarError::{NoPath, FrontierLimit, StateLimit, NanCost};
+
+/// Searches for sequence of actions, which lead from state `s0` to goal state.
+/// Goal state is a state `s`, such that `fend(s)` returns true.
+/// `fnxt` closure should return vector of tuples (action, successor state, cost) for given state.
+/// `fheu` closure should return estimated cost of reaching goal state for given state. Heuristic function must be admissible and constistent.
+pub fn astar_opt_ex<S,A,Fnxt,Fend,Fheu>(s0 : S, fnxt : Fnxt, fend : Fend, fheu : Fheu, 
+                                        arena_cap: usize, frontier_max_cnt: usize, state_max_cnt: usize
+                                        ) -> Result<Vec<A>,AstarError<A>>
+    where S : Ord, 
+        A : Clone,
+        Fnxt : Fn(&S) -> Vec<(A,S,f32)>,
+        Fend : Fn(&S) -> bool,
+        Fheu : Fn(&S) -> f32  {
+
+    let state_arena=TypedArena::<S>::with_capacity(arena_cap);
+    let mut frontier=BinaryHeap::<Rc<FrontierElem<A,S>>>::new();
+    let mut visited=BTreeSet::<&S>::new();
+    let mut stored_state_count: usize = 0;
+    let rs0 = state_arena.alloc(s0);
+    frontier.push(Rc::new(FrontierElem::<A,S>{prev:None, action:None, state:rs0, cost:0.0, total_cost:0.0}));
+    while let Some(fnode)=frontier.pop() {
+        let cstate=(*fnode).state;
+        if fend(cstate) {
+            return Ok(collect_actions(&fnode));
+        }
+        // never true if frontier_max_cnt equals std::usize::MAX
+        if frontier.len()>frontier_max_cnt {
+            return Err(FrontierLimit(collect_actions(&fnode)));
+        }
+        visited.insert(cstate);
+        let nxts=fnxt(cstate);
+        for (a,s,c) in nxts {
+            if c.is_nan() {
+                return Err(NanCost);
+            }
+            if !visited.contains(&s) {
+                let rs=state_arena.alloc(s);
+                stored_state_count += 1;
+                if stored_state_count>state_max_cnt {
+                    return Err(StateLimit(collect_actions(&fnode)));
+                };
+                let hcost=fheu(rs);
+                frontier.push(Rc::new(FrontierElem{prev: Some(fnode.clone()), action: Some(a), state: rs, cost:(*fnode).cost+c, total_cost:(*fnode).cost+c+hcost}));
+            }
+        }
+    }
+    return Err(NoPath);
 }
         
 pub fn astar_opt<S,A,Fnxt,Fend,Fheu>(s0 : S, fnxt : Fnxt, fend : Fend, fheu : Fheu, opt: AstarOpt) -> Result<Vec<A>,AstarError<A>>
@@ -75,45 +148,17 @@ pub fn astar_opt<S,A,Fnxt,Fend,Fheu>(s0 : S, fnxt : Fnxt, fend : Fend, fheu : Fh
         AOStateCap(c) =>                (std::usize::MAX, c),
         AOMaxFrontierStateCap(mf,c) =>  (mf, c),
     };
-    let state_arena=TypedArena::<S>::with_capacity(init_cap);
-    let mut frontier=BinaryHeap::<Rc<FrontierElem<A,S>>>::new();
-    let mut visited=BTreeSet::<&S>::new();
-    let rs0 = state_arena.alloc(s0);
-    frontier.push(Rc::new(FrontierElem::<A,S>{prev:None, action:None, state:rs0, cost:0.0, total_cost:0.0}));
-    while let Some(fnode)=frontier.pop() {
-        let cstate=(*fnode).state;
-        if fend(cstate) {
-            return Ok(collect_actions(&fnode));
-        }
-        if frontier.len()>max_frontier {
-            return Err(FrontierLimit(collect_actions(&fnode)));
-        }
-        visited.insert(cstate);
-        let nxts=fnxt(cstate);
-        for (a,s,c) in nxts {
-            if !visited.contains(&s) {
-                let rs=state_arena.alloc(s);
-                let hcost=fheu(rs);
-                frontier.push(Rc::new(FrontierElem{prev: Some(fnode.clone()), action: Some(a), state: rs, cost:(*fnode).cost+c, total_cost:(*fnode).cost+c+hcost}));
-            }
-        }
-    }
-    return Err(NoPath);
+    astar_opt_ex(s0, fnxt, fend, fheu, init_cap, max_frontier, std::usize::MAX)
 }
 
-/// Copies action fields from single-linked list to array in reverse order.
-fn collect_actions<A,S>(fel: &Rc<FrontierElem<A,S>>) -> Vec<A>
-    where A:Clone {
-    let mut ret=Vec::<A>::with_capacity(16);
-    let mut cur=fel;
-    while let Some(ref prev)=cur.prev {
-        if let Some(ref action)=cur.action {
-            ret.push(action.clone());
-        };
-        cur=prev;
-    }
-    (&mut ret[..]).reverse();
-    ret
+///
+pub fn astar<S,A,Fnxt,Fend,Fheu>(s0 : S, fnxt : Fnxt, fend : Fend, fheu : Fheu) -> Result<Vec<A>,AstarError<A>>
+    where S : Ord, // State must implement Ord trait for BinaryHeap and BTreeSet
+        A : Clone, // Actions are supposed to be lightweight, thus Clone
+        Fnxt : Fn(&S) -> Vec<(A,S,f32)>, // Action generator function returns vector of triples (action, successor state, cost)
+        Fend : Fn(&S) -> bool, // Destination test function returns true for goal states
+        Fheu : Fn(&S) -> f32  { // Estimated cost of path from the state to a goal. Cost heuristic function should be admissible and consistent
+    astar_opt(s0, fnxt, fend, fheu, AONone)
 }
 
 #[cfg(test)]
@@ -171,7 +216,7 @@ mod tests {
         }
     }
 
-    fn str2vec(s: &str) -> (Maze, usize, usize) {
+    fn str2maze(s: &str) -> (Maze, usize, usize) {
         let mut grid = vec![];
         let mut tgt_x = 0;
         let mut tgt_y = 0;
@@ -300,7 +345,7 @@ mod tests {
 
     #[test]
     fn maze1() {
-        let (maze, sx, sy) = str2vec(
+        let (maze, sx, sy) = str2maze(
 "
 ###############
 #@.#.........##
@@ -320,7 +365,7 @@ mod tests {
 
     #[test]
     fn maze2() {
-        let (maze, sx, sy) = str2vec(
+        let (maze, sx, sy) = str2maze(
 "
 ###############
 #@.#.........##
